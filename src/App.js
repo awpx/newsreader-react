d3dafac2cbeec9b2f586e223bf7c8c7e52ae35bf
awp <awpxwork@gmail.com>
2020-03-04 21:11:00 +0700

    fetch story Ids

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import { getStoryIds } from './services/hackernewsApi'
 
-export const App = () => <p>Hello</p>
+export const App = () => {
+  const [ storyIds, setStoryIds ] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getStoryIds()
+      setStoryIds(data)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <p>{JSON.stringify(storyIds)}</p>
+  )
+}
